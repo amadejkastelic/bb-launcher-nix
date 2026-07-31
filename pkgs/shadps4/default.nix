b9d51099182c3e6e3ec9ad2f1952c1ce2f25f9cf
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-07-31 18:23:01 +0200

    shadps4: fix build

--- pkgs/shadps4/default.nix
+++ pkgs/shadps4/default.nix
@@ -1,30 +1,31 @@
 {
   lib,
-  gcc14Stdenv,
+  clangStdenv,
   fetchFromGitHub,
   makeWrapper,
 
+  nixosTests,
   alsa-lib,
   boost,
+  cli11,
   cmake,
   cryptopp,
-  game-music-emu,
-  glslang,
   ffmpeg,
-  flac,
-  fluidsynth,
   fmt,
+  freetype,
   half,
+  httplib,
   jack2,
   libdecor,
-  libffi,
-  libGL,
+  libpng,
   libpulseaudio,
   libunwind,
   libusb1,
-  libvorbis,
-  libxkbcommon,
-  libxmp,
+  magic-enum,
+  minimp3,
+  miniupnpc,
+  miniz,
+  nlohmann_json,
   libgbm,
   libx11,
   libxcb,
@@ -34,15 +35,14 @@
   libxrandr,
   libxscrnsaver,
   libxtst,
-  magic-enum,
-  mesa,
-  mpg123,
   pipewire,
   pkg-config,
   pugixml,
   rapidjson,
   renderdoc,
   robin-map,
+  sdl3,
+  sdl3-mixer,
   sndio,
   stb,
   toml11,
@@ -50,16 +50,23 @@
   vulkan-headers,
   vulkan-loader,
   vulkan-memory-allocator,
-  wayland,
-  wayland-scanner,
   xbyak,
   xxhash,
-  zenity,
-  zlib-ng,
-  zydis,
+  zlib,
+  nix-update-script,
+
+  withRpc ? true,
 }:
 
-gcc14Stdenv.mkDerivation (finalAttrs: {
+let
+  abseilCppSrc = fetchFromGitHub {
+    owner = "abseil";
+    repo = "abseil-cpp";
+    tag = "20250512.1";
+    hash = "sha256-eB7OqTO9Vwts9nYQ/Mdq0Ds4T1KgmmpYdzU09VPWOhk=";
+  };
+in
+clangStdenv.mkDerivation (finalAttrs: {
   pname = "shadps4";
   version = "0.17.0";
 
@@ -67,15 +74,30 @@ gcc14Stdenv.mkDerivation (finalAttrs: {
     owner = "shadps4-emu";
     repo = "shadPS4";
     tag = "v.${finalAttrs.version}";
-    hash = "sha256-y/5dLO1icNoWnJihifvDhfOtF7tvQ1j1sffC9Pa2iUc=";
-    fetchSubmodules = true;
+    hash = "sha256-lNUc1nhubGbs4j/cM+/psbtrL1T4JN7ywqN5aQzdby4=";
 
-    leaveDotGit = true;
-    postFetch = ''
-      cd "$out"
-      git rev-parse --short=8 HEAD > $out/COMMIT
-      date -u -d "@$(git log -1 --pretty=%ct)" "+%Y-%m-%dT%H:%M:%SZ" > $out/SOURCE_DATE_EPOCH
-      find "$out" -name .git -print0 | xargs -0 rm -rf
+    postCheckout = ''
+      git -C "$out" rev-parse --short=8 HEAD > $out/COMMIT
+      date -u -d "@$(git -C "$out" log -1 --pretty=%ct)" "+%Y-%m-%dT%H:%M:%SZ" > $out/SOURCE_DATE_EPOCH
+
+      git -C "$out/externals" submodule update --init --recursive \
+        glslang \
+        zydis \
+        sirit \
+        tracy \
+        libusb \
+        discord-rpc \
+        hwinfo \
+        openal-soft \
+        dear_imgui \
+        LibAtrac9 \
+        aacdec/fdk-aac \
+        spdlog \
+        libressl \
+        ImGuiFileDialog \
+        protobuf \
+        zarchive \
+        zstd
     '';
   };
 
@@ -84,32 +106,32 @@ gcc14Stdenv.mkDerivation (finalAttrs: {
       --replace-fail @APP_VERSION@ ${finalAttrs.version} \
       --replace-fail @GIT_REV@ $(cat COMMIT) \
       --replace-fail @GIT_BRANCH@ ${finalAttrs.version} \
-      --replace-fail @GIT_DESC@ bloodborne-nix \
+      --replace-fail @GIT_DESC@ nixpkgs \
       --replace-fail @BUILD_DATE@ $(cat SOURCE_DATE_EPOCH)
   '';
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    makeWrapper
+  ];
 
   buildInputs = [
     alsa-lib
     boost
+    cli11
     cryptopp
-    game-music-emu
-    glslang
     ffmpeg
-    flac
-    fluidsynth
     fmt
+    freetype
     half
+    httplib
     jack2
     libdecor
-    libffi
-    libGL
+    libpng
     libpulseaudio
     libunwind
     libusb1
-    libvorbis
-    libxkbcommon
-    libxmp
-    libgbm
     libx11
     libxcb
     libxcursor
@@ -119,13 +141,18 @@ gcc14Stdenv.mkDerivation (finalAttrs: {
     libxscrnsaver
     libxtst
     magic-enum
-    mesa
-    mpg123
+    minimp3
+    miniupnpc
+    miniz
+    libgbm
+    nlohmann_json
     pipewire
     pugixml
     rapidjson
     renderdoc
     robin-map
+    sdl3
+    sdl3-mixer
     sndio
     stb
     toml11
@@ -133,63 +160,47 @@ gcc14Stdenv.mkDerivation (finalAttrs: {
     vulkan-headers
     vulkan-loader
     vulkan-memory-allocator
-    wayland
     xbyak
     xxhash
-    zlib-ng
-    zydis
-  ];
-
-  nativeBuildInputs = [
-    cmake
-    makeWrapper
-    pkg-config
-    wayland-scanner
+    zlib
   ];
 
   cmakeFlags = [
+    (lib.cmakeBool "ENABLE_DISCORD_RPC" withRpc)
+    (lib.cmakeBool "ENABLE_TESTS" false)
     (lib.cmakeBool "ENABLE_UPDATER" false)
-    (lib.cmakeBool "SDL_WAYLAND" true)
-    (lib.cmakeBool "SDL_WAYLAND_SHARED" false)
+    (lib.cmakeBool "ENABLE_SYSTEM_LIBRARIES" true)
+    (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_ABSL" "${abseilCppSrc}")
   ];
 
+  # Still in development, help with debugging
   cmakeBuildType = "RelWithDebugInfo";
   dontStrip = true;
 
-  installPhase = ''
-    runHook preInstall
-
-    install -D -t $out/bin shadps4
-    install -Dm644 $src/.github/shadps4.png $out/share/icons/hicolor/512x512/apps/net.shadps4.shadPS4.png
-    install -Dm644 -t $out/share/applications $src/dist/net.shadps4.shadPS4.desktop
-    install -Dm644 -t $out/share/metainfo $src/dist/net.shadps4.shadPS4.metainfo.xml
-
+  postInstall = ''
     wrapProgram $out/bin/shadps4 \
       --prefix LD_LIBRARY_PATH : ${
         lib.makeLibraryPath [
           libpulseaudio
           pipewire
-          wayland
-          libxkbcommon
-          libGL
-          mesa
         ]
-      } \
-      --prefix PATH : ${lib.makeBinPath [ zenity ]}
-
-    runHook postInstall
+      }
   '';
 
   runtimeDependencies = [
     vulkan-loader
     libxi
-    wayland
-    libxkbcommon
-    libGL
-    mesa
   ];
 
-  passthru.updateScript = ./update.sh;
+  passthru = {
+    tests.openorbis-example = nixosTests.shadps4;
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "v\\.(.*)"
+      ];
+    };
+  };
 
   meta = {
     description = "Early in development PS4 emulator";
